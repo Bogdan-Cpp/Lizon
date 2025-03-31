9e8da06281c15049f42a7b936fe4e7dbe04ec604
Bogdan-Cpp <bogdan1000leitan2007@gmail.com>
2025-03-31 20:14:32 +0300

    update 1.7 (VSCode time tracker)

--- mframe.cpp
+++ mframe.cpp
@@ -1,26 +1,54 @@
 #include <wx/wx.h>
+#include <wx/timer.h>
+#include <wx/image.h>
+#include <wx/bitmap.h>
+#include <wx/statbmp.h>
 #include <iostream>
 #include <cstdlib>
 #include "mframe.h"
 #include "main.h"
 
 bool isKeyOff = false;
+int timeSpent = 0;
+int minutes = 0;
 
 mainFrame::mainFrame(const wxString& title) : wxFrame(nullptr, wxID_ANY, title, wxDefaultPosition, wxSize(size_x, size_y), wxDEFAULT_FRAME_STYLE & ~(wxRESIZE_BORDER | wxMAXIMIZE_BOX)){
     SetBackgroundColour(wxColour(30, 30, 30));
     SetTransparent(230);
-
-    wxPanel *panel = new wxPanel(this, wxID_ANY);
     
+    wxPanel *panel = new wxPanel(this, wxID_ANY);
+    wxInitAllImageHandlers();
+    
+    //imagini
+    wxImage vsIcon;
+    if(vsIcon.LoadFile("../vscode.png")){
+        wxImage scaledImage = vsIcon.Rescale(70, 70, wxIMAGE_QUALITY_HIGH);
+        wxBitmap bitmap(vsIcon);
+        wxStaticBitmap *bitmapControl = new wxStaticBitmap(panel, wxID_ANY, bitmap, wxPoint(50, 600), wxSize(wxDefaultSize));
+    }
+    else{
+        return;
+    }
+
+    //time track
+    std::cout << timeSpent << '\n';
+    timerText = new wxStaticText(panel, wxID_ANY, wxString::Format("%d s", timeSpent), wxPoint(150, 630));
+    timer.SetOwner(this);
+    Bind(wxEVT_TIMER, &mainFrame::OnTimer, this);
+    timer.Start(1000);
+    
+    //command line
     commandLine = new wxTextCtrl(panel, wxID_ANY, "", wxPoint(150, 50), wxSize(300, 40), wxTE_PROCESS_ENTER | wxBORDER_NONE);
     commandLine->SetBackgroundColour(wxColour(50, 50, 50));
     commandLine->SetForegroundColour(wxColour(250, 250, 250));
     commandLine->SetFont(wxFont(10, wxFONTFAMILY_DEFAULT, wxFONTSTYLE_NORMAL, wxFONTWEIGHT_BOLD));
-
+    
+    //checkbox
     wxFont font(11, wxFONTFAMILY_DEFAULT, wxFONTSTYLE_NORMAL, wxFONTWEIGHT_BOLD);
     wxCheckBox *keyboard = new wxCheckBox(panel, wxID_ANY, "Keyboard Cleaning  ", wxPoint(200, 120), wxSize(250, 25), wxCHK_CHECKED);
     keyboard->SetFont(font);
     keyboard->Bind(wxEVT_CHECKBOX, &mainFrame::isKeyboardOff, this);
+    
     commandLine->Bind(wxEVT_TEXT_ENTER, &mainFrame::readInput, this);
 }
 void mainFrame::readInput(wxCommandEvent& event){
@@ -52,5 +80,28 @@ void mainFrame::isKeyboardOff(wxCommandEvent& event){
         system("xinput enable 8");
         system("xinput enable 11");
         system("xinput enable 5");
+    }
+}
+
+void mainFrame::OnTimer(wxTimerEvent& event) {
+    const char *command = "xdotool search --name \"Visual Studio Code\"";
+    int rezults = system(command);
+    
+    if(rezults == 0){
+        if(timeSpent == 60){
+            minutes += 1;
+            timeSpent = 0;
+        }
+        timeSpent += 1;
+        if(timerText != nullptr){
+            timerText->SetLabel(wxString::Format("%d m", minutes));
+            std::cout << timeSpent << '\n';
+        }
+        else{
+            std::cerr << "erroare" << '\n';
+        }
+    }
+    else{
+        std::cout << "nu merge";
     }
 }
--- mframe.h
+++ mframe.h
@@ -5,14 +5,19 @@
 #include <cstdlib>
 
 extern bool isKeyOff;
+extern int timeSpent;
+extern int minutes;
 
 class mainFrame : public wxFrame{
     public:
     wxTextCtrl* commandLine;
+    wxStaticText* timerText;
+    wxTimer timer;
     mainFrame(const wxString& title);
 
     void readInput(wxCommandEvent& event);
     void isKeyboardOff(wxCommandEvent& event);
+    void OnTimer(wxTimerEvent& event);
 };
 
 #endif
